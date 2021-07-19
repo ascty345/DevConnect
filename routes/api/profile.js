515a76122f49cf770641176391b3ab72c1e55d2d
Tue Dinh <tuebill28@gmail.com>
2021-07-19 22:12:16 +0700

    add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -159,4 +159,39 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put(
+  '/experience',
+  auth,
+  body('title', 'Title is required').not().isEmpty(),
+  body('company', 'Company is required').not().isEmpty(),
+  body('from', 'From date is required').not().isEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } =
+      req.body;
+
+    const newExp = { title, company, location, from, to, current, description };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ msg: 'Server Error' });
+    }
+  }
+);
+
 module.exports = router;
