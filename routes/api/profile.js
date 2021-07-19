334a67e58882987f0fdcb147715e50dbd32275a1
Tue Dinh <tuebill28@gmail.com>
2021-07-19 21:18:05 +0700

    delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -140,4 +140,23 @@ router.get('/user/:user_id', async (req, res) => {
   }
 });
 
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async (req, res) => {
+  try {
+    // @todo - remove user's posts
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: 'User and Profile Deleted' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
