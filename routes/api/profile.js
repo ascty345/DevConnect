e7a7bb473db7a076525aaaa4189f333992ba65e1
Tue Dinh <tuebill28@gmail.com>
2021-07-20 13:25:47 +0700

    delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -194,4 +194,27 @@ router.put(
   }
 );
 
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Remove experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: 'Server Error' });
+  }
+});
+
 module.exports = router;
