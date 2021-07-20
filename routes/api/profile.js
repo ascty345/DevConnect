0e959fff181cca29d6afe94fda634df69993ea11
Tue Dinh <tuebill28@gmail.com>
2021-07-20 18:13:51 +0700

    delete profile and user will del posts

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const config = require('config');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current users profile
@@ -148,7 +149,13 @@ router.get('/user/:user_id', async (req, res) => {
 router.delete('/', auth, async (req, res) => {
   try {
     // @todo - remove user's posts
-
+    await Post.bulkWrite([
+      {
+        deleteMany: {
+          filter: { user: req.user.id },
+        },
+      },
+    ]);
     // Remove profile
     await Profile.findOneAndRemove({ user: req.user.id });
     // Remove user
