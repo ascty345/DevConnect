f89b63335c9ea89706a81cab212ed75b0687edb6
Tue Dinh <tuebill28@gmail.com>
2021-07-28 20:04:42 +0700

    display Profile Experience & Education

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -101,6 +101,5 @@ export const login = (email, password) => async (dispatch) => {
 // Logout / Clear Profile
 
 export const logout = () => (dispatch) => {
-  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
---
+++ client/src/components/profile/ProfileExperience.js
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+import Moment from 'react-moment';
+
+const ProfileExperience = (props) => {
+  const { company, title, location, current, to, from, description } = props.experience;
+
+  return (
+    <div>
+      <h3 className="text-dark">{company}</h3>
+      <p>
+        <Moment format="YYYY/MM/DD">{from}</Moment> -{' '}
+        {!to ? 'Now' : <Moment format="YYYY/MM/DD">{to}</Moment>}
+      </p>
+      <p>
+        <strong>Position: </strong> {title}
+      </p>
+      {description ? (
+        <p>
+          <strong>Description: </strong> {description}
+        </p>
+      ) : (
+        <Fragment />
+      )}
+    </div>
+  );
+};
+
+export default ProfileExperience;
