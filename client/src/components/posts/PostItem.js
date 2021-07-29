7e0353912b5771f0c38198dc03707a914ea8801a
Tue Dinh <tuebill28@gmail.com>
2021-07-29 15:56:49 +0700

    like and unlike feature

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,11 +1,22 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Moment from 'react-moment';
 
+import { addLike, removeLike } from '../../actions/post';
+
 const PostItem = (props) => {
+  const dispatch = useDispatch();
   const auth = useSelector((state) => state.auth);
   const { _id, text, name, avatar, user, likes, comments, date } = props.post;
+
+  const likeHandler = () => {
+    dispatch(addLike(_id));
+  };
+
+  const unlikeHandler = () => {
+    dispatch(removeLike(_id));
+  };
 
   return (
     <div className="post bg-white p-1 my-1">
@@ -20,11 +31,11 @@ const PostItem = (props) => {
         <p className="post-date">
           Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
         </p>
-        <button type="button" className="btn btn-light">
+        <button onClick={likeHandler} type="button" className="btn btn-light">
           <i className="fas fa-thumbs-up" />{' '}
           <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
         </button>
-        <button type="button" className="btn btn-light">
+        <button onClick={unlikeHandler} type="button" className="btn btn-light">
           <i className="fas fa-thumbs-down"></i>
         </button>
         <Link to={`post/${_id}`} className="btn btn-primary">
